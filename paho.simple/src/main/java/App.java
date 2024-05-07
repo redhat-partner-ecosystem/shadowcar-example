62335711826233c94955e9f250ad3e6fdfc2ed0c
Michael Kuehl <hello@txs.vc>
2024-05-07 12:03:25 +0000

    started a new sample

---
+++ paho.simple/src/main/java/App.java
@@ -0,0 +1,10 @@
+package shadowcar.example.paho.simple;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+//import org.eclipse.paho.sample.mqttclient.mqttv3.MqttV3Executor;
+//import org.eclipse.paho.sample.mqttclient.mqttv5.MqttV5Executor;
